b8d093277b2fed1e8a1eeb251e7d1757c2e883dc
Icaro Muniz <icaro.muniz@gmail.com>
2019-02-22 15:25:11 +1000

    browser init

---
+++ process/src/main/java/au/com/envirotech/process/ProcComp.java
@@ -0,0 +1,14 @@
+package au.com.envirotech.process;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcComp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcComp.class, args);
+		System.out.println("123 testando!");
+	}
+
+}
